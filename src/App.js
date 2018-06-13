ea78ff16c026a5ce83a74e761e653dca133c891c
MVladimir Titskiy <catindev@gmail.com>
2018-06-13 21:04:39 +0600

    Initial bullshit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,21 @@ import React, { Component } from 'react'
 import { BrowserRouter, Route, Redirect } from "react-router-dom"
 import './App.css';
 
+import AsyncComponent from './Components/AsyncComponent/AsyncComponent.js'
 import Header from './Components/Header/Header.js'
 import Sidebar from './Components/Sidebar/Sidebar.js'
-import IcomingCalls from './Components/IcomingCalls/IcomingCalls'
+// import IncomingCalls from './Components/IncomingCalls/IncomingCalls'
 import MissingCalls from './Components/MissingCalls/MissingCalls'
+
+const IncomingCalls = AsyncComponent(
+  () => import('./Components/IncomingCalls/IncomingCalls')
+    .then(module => module.default),
+  { name: 'IncomingCalls' },
+)
 
 
 class App extends Component {
-  state = { title: 'TEST-O' }
-
-  handleUpdate() {
-    console.log('action', this.props)
-    // let {
-    //   action
-    // } = this.state.location;
-
-    // console.log('action', this.props)
-
-    // if (action === 'PUSH') {
-    //   window.scrollTo(0, 0);
-    // }
-  }
+  state = { title: 'ТОО "Рога & Co" Ltd' }
 
   render() {
     const { title } = this.state
@@ -33,10 +27,10 @@ class App extends Component {
           <article className="App">
             <Sidebar />
             <Route exact path="/" render={() => <Redirect to="/callcenter/all" />} />
-            <Route path="/callcenter/all" component={IcomingCalls} onEnter={this.handleUpdate.bind(this)} />
-            <Route path="/callcenter/incoming" component={IcomingCalls} />
+            <Route path="/callcenter/all" component={IncomingCalls} />
+            <Route path="/callcenter/incoming" component={IncomingCalls} />
             <Route path="/callcenter/missing" component={MissingCalls} />
-            <Route path="/callcenter/reaction" component={IcomingCalls} />
+            <Route path="/callcenter/reaction" component={IncomingCalls} />
           </article>
         </div>
       </BrowserRouter>
